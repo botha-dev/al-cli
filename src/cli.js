de3aeddc63820b811995db6cfb2cfdbf3c6bf6ca
botha-dev <jacobotha16@gmail.com>
2021-01-09 21:49:07 +0200

    init Commit

---
+++ src/cli.js
@@ -0,0 +1,37 @@
+import { createObject } from './main';
+
+const yargs = require('yargs');
+
+export async function cli(args)
+{    
+    const options = yargs
+    .options({
+        type: {
+            alias: 't',
+            choices: ['table','page','report','codeunit'],
+            demandOption: true,
+            description: 'Object Type to create.'
+        },
+        name: {
+            alias: 'n', 
+            demandOption: true, 
+            description: 'Object Name to create.'
+
+        },
+        id : {
+            alias:'i',
+            type: 'number',
+            default: 50100, 
+            description: 'Object ID to create.'
+        },
+        extends: {
+            alias: 'e',             
+            description: 'Object Name to extend.'
+
+        },
+    }).argv;
+
+    await createObject(options);
+    
+    //console.log(options);
+}
---
+++ src/main.js
@@ -0,0 +1,117 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import ncp from 'ncp';
+import path from 'path';
+import { promisify } from 'util';
+import { option } from 'yargs';
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+const fsPromise = require('fs').promises;
+const fs2 = require('fs');
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+async function copyTemplateFiles(options) {
+
+    return fsPromise.readdir(options.templateDirectory)
+        .then(async filenames => {
+            for (let filename of filenames) {
+                await handleTemplateFile(options, filename);
+                //console.log(filename)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
+
+async function handleTemplateFile(options, file) {
+
+    var sourceFile = path.join(options.templateDirectory, file);
+    var destFile = path.join(options.targetDirectory, handleFileName(options, file))
+    //console.log(sourceFile);
+    try {
+        await fsPromise.copyFile(
+            sourceFile,
+            destFile,
+            fs.constants.COPYFILE_EXCL | fs.constants.COPYFILE_FICLONE
+        );
+
+        console.log("%s  %s", chalk.green('CREATED'), destFile);
+
+        await fsPromise.readFile(destFile)
+            .then(data => {
+                var newData = data.toString().replace(/<id>/gi, options.id);
+                newData = newData.toString().replace(/<name>/gi, '"' + options.name + '"');
+                newData = newData.toString().replace(/<extends>/gi, '"' + options.extends + '"');
+                
+
+                fsPromise.writeFile(destFile, newData);
+            })
+ 
+    } catch (error) {
+        //console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+        console.error('%s', chalk.red.bold(error));
+    }
+
+}
+
+
+
+function handleFileName(options, file) {
+    var tempName = options.name;
+    var tempNameSplit = tempName.split(' ');
+    console.log(tempNameSplit);
+    var fileName = '';
+
+    if (tempNameSplit.length > 1) {
+        for (let index = 0; index < tempNameSplit.length; index++) {
+            const element = capitalize(tempNameSplit[index]);
+            fileName += element;
+        }
+    } else {
+        fileName = tempName;
+    }
+
+    fileName = fileName + file;
+    return fileName;
+}
+
+export async function createObject(options) {
+    options = {
+        ...options,
+        targetDirectory: options.targetDirectory || process.cwd(),
+    };
+
+    const currentFileUrl = import.meta.url;
+
+    var templateDir = path.join(
+        new URL(currentFileUrl).pathname,
+        '../../templates',
+        options.type.toLowerCase()
+
+    );
+
+    templateDir = templateDir.substr(1, templateDir.length);
+
+    options.templateDirectory = templateDir;
+
+    try {
+        await access(templateDir, fs.constants.R_OK);
+    } catch (err) {
+        console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+        process.exit(1);
+    }
+
+    console.log('creating template files');
+    await copyTemplateFiles(options);
+
+    //console.log('creating template files');
+    //await copyTemplateFiles(options);
+
+    console.log('%s Object created.', chalk.green.bold('DONE'));
+    return true;
+}
